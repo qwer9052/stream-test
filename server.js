2ec09963d5e8638bcf0991b7ee2d8235bfc1a9a1
qwer9052 <qwer9052@gmail.com>
2023-11-06 13:48:54 +0900

    a

--- server.js
+++ server.js
@@ -1,76 +1,74 @@
-let express = require('express');
-let http = require('http');
-let app = express();
-let cors = require('cors');
-let server = http.createServer(app);
-let socketio = require('socket.io');
-let io = socketio.listen(server);
+let express = require('express')
+let http = require('http')
+let app = express()
+let cors = require('cors')
+let server = http.createServer(app)
+let socketio = require('socket.io')
+let io = socketio.listen(server)
 
-app.use(cors());
-const PORT = process.env.PORT || 8080;
+app.use(cors())
+const PORT = process.env.PORT || 8080
 
-let users = {};
+let users = {}
 
-let socketToRoom = {};
+let socketToRoom = {}
 
-const maximum = process.env.MAXIMUM || 4;
+const maximum = process.env.MAXIMUM || 4
 
-io.on('connection', socket => {
-    socket.on('join_room', data => {
-        if (users[data.room]) {
-            const length = users[data.room].length;
-            if (length === maximum) {
-                socket.to(socket.id).emit('room_full');
-                return;
-            }
-            users[data.room].push({id: socket.id, email: data.email});
-        } else {
-            users[data.room] = [{id: socket.id, email: data.email}];
-        }
-        socketToRoom[socket.id] = data.room;
+io.on('connection', (socket) => {
+  socket.on('join_room', (data) => {
+    console.log('connection', data)
+    if (users[data.room]) {
+      const length = users[data.room].length
+      if (length === maximum) {
+        socket.to(socket.id).emit('room_full')
+        return
+      }
+      users[data.room].push({ id: socket.id, email: data.email, auth: data.auth })
+    } else {
+      users[data.room] = [{ id: socket.id, email: data.email, auth: data.auth }]
+    }
+    socketToRoom[socket.id] = data.room
 
-        socket.join(data.room);
-        console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
+    socket.join(data.room)
+    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`)
 
-        const usersInThisRoom = users[data.room].filter(user => user.id !== socket.id);
+    const usersInThisRoom = users[data.room].filter((user) => user.id !== socket.id)
 
-        console.log(usersInThisRoom);
+    console.log(usersInThisRoom)
 
-        io.sockets.to(socket.id).emit('all_users', usersInThisRoom);
-    });
+    io.sockets.to(socket.id).emit('all_users', usersInThisRoom)
+  })
 
-    socket.on('offer', data => {
-        //console.log(data.sdp);
-        socket.to(data.offerReceiveID).emit('getOffer', {sdp: data.sdp, offerSendID: data.offerSendID, offerSendEmail: data.offerSendEmail});
-    });
+  socket.on('offer', (data) => {
+    socket.to(data.offerReceiveID).emit('getOffer', { sdp: data.sdp, offerSendID: data.offerSendID, offerSendEmail: data.offerSendEmail })
+  })
 
-    socket.on('answer', data => {
-        //console.log(data.sdp);
-        socket.to(data.answerReceiveID).emit('getAnswer', {sdp: data.sdp, answerSendID: data.answerSendID});
-    });
+  socket.on('answer', (data) => {
+    socket.to(data.answerReceiveID).emit('getAnswer', { sdp: data.sdp, answerSendID: data.answerSendID })
+  })
 
-    socket.on('candidate', data => {
-        //console.log(data.candidate);
-        socket.to(data.candidateReceiveID).emit('getCandidate', {candidate: data.candidate, candidateSendID: data.candidateSendID});
-    })
+  socket.on('candidate', (data) => {
+    socket.to(data.candidateReceiveID).emit('getCandidate', { candidate: data.candidate, candidateSendID: data.candidateSendID })
+  })
 
-    socket.on('disconnect', () => {
-        console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter(user => user.id !== socket.id);
-            users[roomID] = room;
-            if (room.length === 0) {
-                delete users[roomID];
-                return;
-            }
-        }
-        socket.to(roomID).emit('user_exit', {id: socket.id});
-        console.log(users);
-    })
-});
+  socket.on('disconnect', () => {
+    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`)
+    const roomID = socketToRoom[socket.id]
+    let room = users[roomID]
+    if (room) {
+      room = room.filter((user) => user.id !== socket.id)
+      users[roomID] = room
+      if (room.length === 0) {
+        delete users[roomID]
+        return
+      }
+    }
+    socket.to(roomID).emit('user_exit', { id: socket.id })
+    console.log(users)
+  })
+})
 
 server.listen(PORT, () => {
-    console.log(`server running on ${PORT}`);
-});
+  console.log(`server running on ${PORT}`)
+})
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,216 +2,56 @@ import React, { useState, useRef, useEffect, useCallback } from 'react';
 import io from 'socket.io-client';
 import Video from './Components/Video';
 import { WebRTCUser } from './types';
-
-const pc_config = {
-	iceServers: [
-		// {
-		//   urls: 'stun:[STUN_IP]:[PORT]',
-		//   'credentials': '[YOR CREDENTIALS]',
-		//   'username': '[USERNAME]'
-		// },
-		{
-			urls: 'stun:stun.l.google.com:19302',
-		},
-	],
-};
-const SOCKET_SERVER_URL = 'http://localhost:8080';
+import VideoApp from './VideoApp';
+import { Button } from '@material-ui/core';
 
 const App = () => {
-	const socketRef = useRef<SocketIOClient.Socket>();
-	const pcsRef = useRef<{ [socketId: string]: RTCPeerConnection }>({});
-	const localVideoRef = useRef<HTMLVideoElement>(null);
-	const localStreamRef = useRef<MediaStream>();
-	const [users, setUsers] = useState<WebRTCUser[]>([]);
+	const [auth, setAuth] = useState<'admin' | 'user'>();
+	// 상태 변수를 사용하여 입력된 텍스트를 관리
+	const [email, setEmail] = useState();
 
-	const getLocalStream = useCallback(async () => {
-		try {
-			const localStream = await navigator.mediaDevices.getUserMedia({
-				audio: true,
-				video: {
-					width: 240,
-					height: 240,
-				},
-			});
-			localStreamRef.current = localStream;
-			if (localVideoRef.current) localVideoRef.current.srcObject = localStream;
-			if (!socketRef.current) return;
-			socketRef.current.emit('join_room', {
-				room: '1234',
-				email: 'sample@naver.com',
-			});
-		} catch (e) {
-			console.log(`getUserMedia error: ${e}`);
-		}
-	}, []);
-
-	const createPeerConnection = useCallback((socketID: string, email: string) => {
-		try {
-			const pc = new RTCPeerConnection(pc_config);
-
-			pc.onicecandidate = (e) => {
-				if (!(socketRef.current && e.candidate)) return;
-				console.log('onicecandidate');
-				socketRef.current.emit('candidate', {
-					candidate: e.candidate,
-					candidateSendID: socketRef.current.id,
-					candidateReceiveID: socketID,
-				});
-			};
-
-			pc.oniceconnectionstatechange = (e) => {
-				console.log(e);
-			};
-
-			pc.ontrack = (e) => {
-				console.log('ontrack success');
-				setUsers((oldUsers) =>
-					oldUsers
-						.filter((user) => user.id !== socketID)
-						.concat({
-							id: socketID,
-							email,
-							stream: e.streams[0],
-						}),
-				);
-			};
-
-			if (localStreamRef.current) {
-				console.log('localstream add');
-				localStreamRef.current.getTracks().forEach((track) => {
-					if (!localStreamRef.current) return;
-					pc.addTrack(track, localStreamRef.current);
-				});
-			} else {
-				console.log('no local stream');
-			}
-
-			return pc;
-		} catch (e) {
-			console.error(e);
-			return undefined;
-		}
-	}, []);
-
-	useEffect(() => {
-		socketRef.current = io.connect(SOCKET_SERVER_URL);
-		getLocalStream();
-
-		socketRef.current.on('all_users', (allUsers: Array<{ id: string; email: string }>) => {
-			allUsers.forEach(async (user) => {
-				if (!localStreamRef.current) return;
-				const pc = createPeerConnection(user.id, user.email);
-				if (!(pc && socketRef.current)) return;
-				pcsRef.current = { ...pcsRef.current, [user.id]: pc };
-				try {
-					const localSdp = await pc.createOffer({
-						offerToReceiveAudio: true,
-						offerToReceiveVideo: true,
-					});
-					console.log('create offer success');
-					await pc.setLocalDescription(new RTCSessionDescription(localSdp));
-					socketRef.current.emit('offer', {
-						sdp: localSdp,
-						offerSendID: socketRef.current.id,
-						offerSendEmail: 'offerSendSample@sample.com',
-						offerReceiveID: user.id,
-					});
-				} catch (e) {
-					console.error(e);
-				}
-			});
-		});
-
-		socketRef.current.on(
-			'getOffer',
-			async (data: {
-				sdp: RTCSessionDescription;
-				offerSendID: string;
-				offerSendEmail: string;
-			}) => {
-				const { sdp, offerSendID, offerSendEmail } = data;
-				console.log('get offer');
-				if (!localStreamRef.current) return;
-				const pc = createPeerConnection(offerSendID, offerSendEmail);
-				if (!(pc && socketRef.current)) return;
-				pcsRef.current = { ...pcsRef.current, [offerSendID]: pc };
-				try {
-					await pc.setRemoteDescription(new RTCSessionDescription(sdp));
-					console.log('answer set remote description success');
-					const localSdp = await pc.createAnswer({
-						offerToReceiveVideo: true,
-						offerToReceiveAudio: true,
-					});
-					await pc.setLocalDescription(new RTCSessionDescription(localSdp));
-					socketRef.current.emit('answer', {
-						sdp: localSdp,
-						answerSendID: socketRef.current.id,
-						answerReceiveID: offerSendID,
-					});
-				} catch (e) {
-					console.error(e);
-				}
-			},
-		);
-
-		socketRef.current.on(
-			'getAnswer',
-			(data: { sdp: RTCSessionDescription; answerSendID: string }) => {
-				const { sdp, answerSendID } = data;
-				console.log('get answer');
-				const pc: RTCPeerConnection = pcsRef.current[answerSendID];
-				if (!pc) return;
-				pc.setRemoteDescription(new RTCSessionDescription(sdp));
-			},
-		);
-
-		socketRef.current.on(
-			'getCandidate',
-			async (data: { candidate: RTCIceCandidateInit; candidateSendID: string }) => {
-				console.log('get candidate');
-				const pc: RTCPeerConnection = pcsRef.current[data.candidateSendID];
-				if (!pc) return;
-				await pc.addIceCandidate(new RTCIceCandidate(data.candidate));
-				console.log('candidate add success');
-			},
-		);
-
-		socketRef.current.on('user_exit', (data: { id: string }) => {
-			if (!pcsRef.current[data.id]) return;
-			pcsRef.current[data.id].close();
-			delete pcsRef.current[data.id];
-			setUsers((oldUsers) => oldUsers.filter((user) => user.id !== data.id));
-		});
-
-		return () => {
-			if (socketRef.current) {
-				socketRef.current.disconnect();
-			}
-			users.forEach((user) => {
-				if (!pcsRef.current[user.id]) return;
-				pcsRef.current[user.id].close();
-				delete pcsRef.current[user.id];
-			});
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [createPeerConnection, getLocalStream]);
+	// 입력 필드의 값이 변경될 때 호출되는 함수
+	const handleInputChange = (event: any) => {
+		setEmail(event.target.value);
+	};
 
 	return (
 		<div>
-			<video
-				style={{
-					width: 240,
-					height: 240,
-					margin: 5,
-					backgroundColor: 'black',
-				}}
-				muted
-				ref={localVideoRef}
-				autoPlay
-			/>
-			{users.map((user, index) => (
-				<Video key={index} email={user.email} stream={user.stream} />
-			))}
+			{auth && email ? (
+				<VideoApp auth={auth} email={email} />
+			) : (
+				<div style={{ display: 'flex' }}>
+					<input
+						type="email"
+						value={email}
+						onChange={handleInputChange}
+						placeholder="다르게 이메일 입력"
+					/>
+					<Button
+						style={{
+							width: 300,
+							height: 100,
+							color: '#000',
+							backgroundColor: '#ffaadd',
+						}}
+						onClick={() => setAuth('admin')}
+					>
+						영상켜기
+					</Button>
+					<Button
+						style={{
+							width: 300,
+							height: 100,
+							color: '#000',
+							backgroundColor: '#aaaacc',
+						}}
+						title="게스트 참여"
+						onClick={() => setAuth('user')}
+					>
+						게스트 참여
+					</Button>
+				</div>
+			)}
 		</div>
 	);
 };
